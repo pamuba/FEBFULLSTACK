377fc296b025c783c5e5ee3b04d7af31e2c7b539
pamuba <partha.bora24@gmail.com>
2022-03-09 20:33:25 +0000

    22

--- arrowFns.js
+++ arrowFns.js
@@ -4,8 +4,27 @@
 //functions(objects) can be returned from other funs
 //functions(objects) can be passed as arguments
 
+//transpilers babel
+//babeljs + canIuse
+
+
 let add = (x,y) => x+y
 console.log(add(3,4))
 
 //1.Arrow fns doesnt have the this keyword
+
+let group = {
+    title:"Our Group",
+    students:["John", "Jill", "Jimmy"],
+
+    showList(){
+        this.students.forEach(
+            (student)=>{
+                console.log(this.title+':'+student)
+            }
+        )
+    }
+}
+
+group.showList()
 
--- dist/arrowFns.dev.js
+++ dist/arrowFns.dev.js
@@ -4,8 +4,23 @@
 //functions are objects
 //functions(objects) can be returned from other funs
 //functions(objects) can be passed as arguments
+//transpilers babel
+//babeljs + canIuse
 var add = function add(x, y) {
   return x + y;
 };
 
 console.log(add(3, 4)); //1.Arrow fns doesnt have the this keyword
+
+var group = {
+  title: "Our Group",
+  students: ["John", "Jill", "Jimmy"],
+  showList: function showList() {
+    var _this = this;
+
+    this.students.forEach(function (student) {
+      console.log(_this.title + ':' + student);
+    });
+  }
+};
+group.showList();
