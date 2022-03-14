301b76735eaaa5f06fd7fd0978b8d18065e3c460
pamuba <partha.bora24@gmail.com>
2022-03-14 20:23:15 +0000

    48

--- asyncawait.js
+++ asyncawait.js
@@ -7,3 +7,32 @@ function doubleAfter2Seconds(x){
         )
     })
 }
+// let v1, v2,v3
+// doubleAfter2Seconds(10).then(
+//     v1 => console.log(v1)
+// )
+// doubleAfter2Seconds(20).then(
+//     v2 => console.log(v2)
+// )
+// doubleAfter2Seconds(30).then(
+//     v3 => {console.log(v3)
+//     console.log(v1+v2+v3)}
+// )
+
+// let sum = doubleAfter2Seconds(10) + doubleAfter2Seconds(20) + doubleAfter2Seconds(30)
+//promises can be difficult
+function addPromise(){
+    return new Promise(resolve => {
+        doubleAfter2Seconds(10).then(a=>{
+            doubleAfter2Seconds(20).then(b=>{
+                doubleAfter2Seconds(30).then(c=>{
+                    resolve(a+b+c)
+                })
+            })
+        })
+    })
+}
+
+addPromise().then(r=>{
+    console.log(r)
+})
