34dbde72152178de740aad59537b3fe454bd2565
pamuba <partha.bora24@gmail.com>
2022-03-14 20:48:02 +0000

    410

--- dist/asyncawait.dev.js
+++ dist/asyncawait.dev.js
@@ -6,4 +6,69 @@ function doubleAfter2Seconds(x) {
       resolve(x * 2);
     }, 2000);
   });
+} // let v1, v2,v3
+// doubleAfter2Seconds(10).then(
+//     v1 => console.log(v1)
+// )
+// doubleAfter2Seconds(20).then(
+//     v2 => console.log(v2)
+// )
+// doubleAfter2Seconds(30).then(
+//     v3 => {console.log(v3)
+//     console.log(v1+v2+v3)}
+// )
+// let sum = doubleAfter2Seconds(10) + doubleAfter2Seconds(20) + doubleAfter2Seconds(30)
+//promises can be difficult
+// function addPromise(){
+//     return new Promise(resolve => {
+//         doubleAfter2Seconds(10).then(a=>{
+//             doubleAfter2Seconds(20).then(b=>{
+//                 doubleAfter2Seconds(30).then(c=>{
+//                     resolve(a+b+c)
+//                 })
+//             })
+//         })
+//     })
+// }
+// addPromise().then(r=>{
+//     console.log(r)
+// })
+//async always returns a promise
+//await can be used only inside async fn
+//exec async sync
+
+
+function addAsync() {
+  var a, b, c;
+  return regeneratorRuntime.async(function addAsync$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(doubleAfter2Seconds(10));
+
+        case 2:
+          a = _context.sent;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(doubleAfter2Seconds(20));
+
+        case 5:
+          b = _context.sent;
+          _context.next = 8;
+          return regeneratorRuntime.awrap(doubleAfter2Seconds(30));
+
+        case 8:
+          c = _context.sent;
+          return _context.abrupt("return", a + b + c);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
 }
+
+addAsync().then(function (r) {
+  console.log(r);
+});
---
+++ map_reduce_filter.js
@@ -0,0 +1,127 @@
+var officers = [
+    { id: 20, name: 'Captain Piett' },
+    { id: 24, name: 'General Veers' },
+    { id: 56, name: 'Admiral Ozzel' },
+    { id: 88, name: 'Commander Jerjerrod' }
+  ];
+
+  //[20,24,56, 88]
+
+//   //forEach
+//   var officersId = []
+//   officers.forEach(function(officer){
+//     officersId.push(officer.id)
+//   })
+//   console.log(officersId)
+
+//   var officersIds = officers.map(officer=>officer.id)
+//   console.log(officersIds)
+
+//   for(i of officers)
+//   {
+//       console.log(i)
+//   }
+
+// var pilots = [
+//     {
+//       id: 10,
+//       name: "Poe Dameron",
+//       years: 14,
+//     },
+//     {
+//       id: 2,
+//       name: "Temmin 'Snap' Wexley",
+//       years: 30,
+//     },
+//     {
+//       id: 41,
+//       name: "Tallissan Lintra",
+//       years: 16,
+//     },
+//     {
+//       id: 99,
+//       name: "Ello Asty",
+//       years: 22,
+//     }
+//   ];
+
+//   var totalYears = pilots.reduce((accumulator, pilot)=>{
+//       return accumulator + pilot.years
+//   },0)
+//   console.log(totalYears)
+
+// var pilots = [
+//     {
+//       id: 2,
+//       name: "Wedge Antilles",
+//       faction: "Rebels",
+//     },
+//     {
+//       id: 8,
+//       name: "Ciena Ree",
+//       faction: "Empire",
+//     },
+//     {
+//       id: 40,
+//       name: "Iden Versio",
+//       faction: "Empire",
+//     },
+//     {
+//       id: 66,
+//       name: "Thane Kyrell",
+//       faction: "Rebels",
+//     }
+//   ];
+
+//   var rebels = pilots.filter(pilot => pilot.faction === "Rebels")
+//   var empire = pilots.filter(pilot => pilot.faction === "Empire")
+
+//   console.log(rebels)
+//   console.log(empire)
+
+  //Combine map reduce and filter
+
+//   var personnel = [
+//     {
+//       id: 5,
+//       name: "Luke Skywalker",
+//       pilotingScore: 98,
+//       shootingScore: 56,
+//       isForceUser: true,
+//     },
+//     {
+//       id: 82,
+//       name: "Sabine Wren",
+//       pilotingScore: 73,
+//       shootingScore: 99,
+//       isForceUser: false,
+//     },
+//     {
+//       id: 22,
+//       name: "Zeb Orellios",
+//       pilotingScore: 20,
+//       shootingScore: 59,
+//       isForceUser: false,
+//     },
+//     {
+//       id: 15,
+//       name: "Ezra Bridger",
+//       pilotingScore: 43,
+//       shootingScore: 67,
+//       isForceUser: true,
+//     },
+//     {
+//       id: 11,
+//       name: "Caleb Dume",
+//       pilotingScore: 71,
+//       shootingScore: 85,
+//       isForceUser: true,
+//     },
+//   ];
+
+// //   get the total score of force users
+// const totalJediScore = personnel
+//   .filter(person => person.isForceUser)
+//   .map(jedi => jedi.pilotingScore + jedi.shootingScore)
+//   .reduce((acc, score) => acc + score, 0);
+
