065b9e82fea0f5a80fe34f4e282449effa6e48a8
pamuba <partha.bora24@gmail.com>
2022-03-16 19:47:21 +0000

    63

--- AngularJSDemos/ng-repeat.js
+++ AngularJSDemos/ng-repeat.js
@@ -2,28 +2,13 @@ var myApp = angular.module("myModule",[])
 
 myApp.controller("myController", function($scope){
     var employees = [
-        {
-            name: "Ben", dateOfBirth: new Date("November 23, 1980"),
-            gender: "Male", salary: 55000.788
-        },
-        {
-            name: "Sara", dateOfBirth: new Date("May 05, 1970"),
-            gender: "Female", salary: 68000
-        },
-        {
-            name: "Mark", dateOfBirth: new Date("August 15, 1974"),
-            gender: "Male", salary: 57000
-        },
-        {
-            name: "Pam", dateOfBirth: new Date("October 27, 1979"),
-            gender: "Female", salary: 53000
-        },
-        {
-            name: "Todd", dateOfBirth: new Date("December 30, 1983"),
-            gender: "Male", salary: 60000
-        }
+        { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000 },
+        { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000 },
+        { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000 },
+        { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000 },
+        { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
     ];
-    $scope.country = country
+    $scope.employees = employees
 });
 
 
