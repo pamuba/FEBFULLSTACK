f9544da87cf64712b0f6a7df46c113cecc85440a
pamuba <partha.bora24@gmail.com>
2022-03-17 20:29:52 +0000

    74

---
+++ AngularJSDemos/dist/compile2.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var app = angular.module("app", []);
+app.controller("msg", ['$scope', function ($scope) {}]);
+app.directive('message', function () {
+  return {
+    compile: function compile(tElement, tAttributes) {
+      console.log(tAttributes.text + " In Compile phase");
+      tElement.css("border", "1px solid #C0C0C0");
+      return {
+        pre: function pre(scope, ielement, iattributes) {
+          console.log(iattributes.text + " In Pre phase");
+        },
+        post: function post(scope, ielement, iattributes) {
+          console.log(iattributes.text + " In Post phase");
+        }
+      };
+    },
+    controller: function controller($scope, $element, $attrs) {
+      console.log($attrs.text + " In Controller phase");
+    }
+  };
+});
