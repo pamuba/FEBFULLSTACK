a5cb55acb277917984090b527863f6cb56b7d286
pamuba <partha.bora24@gmail.com>
2022-03-17 20:38:06 +0000

    75

--- AngularJSDemos/dist/compile2.dev.js
+++ AngularJSDemos/dist/compile2.dev.js
@@ -11,13 +11,25 @@ app.directive('message', function () {
         pre: function pre(scope, ielement, iattributes) {
           console.log(iattributes.text + " In Pre phase");
         },
+        //post link fn is the best place to modify DOM
         post: function post(scope, ielement, iattributes) {
           console.log(iattributes.text + " In Post phase");
+
+          if (iattributes.text === "3") {
+            ielement.css("border", "1px solid red");
+          } //best place to access the DPOM
+
+
+          ielement.on("click", scope.btnClick);
         }
       };
     },
     controller: function controller($scope, $element, $attrs) {
-      console.log($attrs.text + " In Controller phase");
+      console.log($attrs.text + " In Controller phase"); //any event handling related to the scope, then it should be provided through the controller
+
+      $scope.btnClick = function () {
+        alert();
+      };
     }
   };
 });
