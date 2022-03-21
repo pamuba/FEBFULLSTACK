49f5f64d992c6704c9f4c3a9cbc252c709ab5269
pamuba <partha.bora24@gmail.com>
2022-03-21 20:25:37 +0000

    77

--- AngularJSDemos/watcher.js
+++ AngularJSDemos/watcher.js
@@ -31,22 +31,44 @@ myApp.controller("myController", function($scope){
 //             $scope.b = $scope.a * 2
 // });
 
-    $scope.o = {
-        a:1,
-        b:2,
-        c:3
-    }
-    //reference watcher
+///////////////////////////////////////watcher with objects
+    // $scope.o = {
+    //     a:1,
+    //     b:2,
+    //     c:3
+    // }
+    // //reference watcher
+    // // $scope.$watch("o", function(newVal, oldVal){
+    // //     if(newVal != oldVal)
+    // //         $scope.o.c = $scope.o.a * $scope.o.a
+    // // });
+
+    // //equility watcher
     // $scope.$watch("o", function(newVal, oldVal){
     //     if(newVal != oldVal)
-    //         $scope.o.c = $scope.o.a * $scope.o.a
-    // });
+    //         $scope.o.c = $scope.o.a * $scope.o.b
+    // }, true);
 
-    //equility watcher
-    $scope.$watch("o", function(newVal, oldVal){
-        if(newVal != oldVal)
-            $scope.o.c = $scope.o.a * $scope.o.b
-    }, true);
+    //watchers with collections
+    
+    $scope.emps = [
+        {empno:"1001", ename:"John"},
+        {empno:"1002", ename:"Jim"},
+        {empno:"1003", ename:"Jill"},
+        {empno:"1004", ename:"Mill"},
+    ]
+
+    $scope.addEmp = function(){
+        var newEmpno = 100 + $scope.emps.lenght + 1;
+        var newName = "ename"+newEmpno
+
+        $scope.emps.push({empno:newEmpno, ename:newName})
+    }
+
+    $scope.modify3rdEmp = function(){
+        $scope.emps[2].ename = "Test"
+    }
+
 })
 
 
