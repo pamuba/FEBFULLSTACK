68373a83a77467aff1a0752db1d37f6450ed24c4
pamuba <partha.bora24@gmail.com>
2022-03-21 21:01:07 +0000

    79

---
+++ AngularJSDemos/watcher2.js
@@ -0,0 +1,40 @@
+var myApp = angular.module("myModule",[])
+
+myApp.controller("myController", function($scope, $rootScope){
+    $scope.a = 10
+    $scope.b = 20
+
+    $scope.calcSum = function(){
+        $scope.s = Number($scope.a) + Number($scope.b)
+    }
+    // $scope.c = 30
+    
+
+    // $scope.$watch("a", function(newVal, oldVal){
+    //     if(newVal != oldVal)
+    //         console.log("A is changed to:"+ newVal)
+    // })
+    // $scope.$watch("b", function(newVal, oldVal){
+    //     if(newVal != oldVal)
+    //         console.log("B is changed to:"+ newVal)
+    // });
+    // $scope.$watch("c", function(newVal, oldVal){
+    //     if(newVal != oldVal)
+    //        { console.log("C is changed to:"+ newVal)
+    //          if($scope.c > 50){
+    //              $scope.a = 1000;
+    //          }
+    //     }
+    // });
+
+    // $rootScope.$watch(function(){
+    //     console.log('--------- diges process started-----------')
+    // })
+
+})
+
+var btnClick = function(){
+    $scope.s = Number($scope.a) + Number($scope.b)
+}
+
+
