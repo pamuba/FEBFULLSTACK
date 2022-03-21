4da49bd97ebe7bfac8745fe20993575ce9286f7d
pamuba <partha.bora24@gmail.com>
2022-03-21 20:03:39 +0000

    73

--- AngularJSDemos/dist/watcher.dev.js
+++ AngularJSDemos/dist/watcher.dev.js
@@ -5,20 +5,23 @@ myApp.controller("myController", function ($scope) {
   $scope.a = 10;
   $scope.b = 20;
   $scope.c = 30;
-  $scope.d = 40;
-
-  $scope.updateC = function () {
-    $scope.c = $scope.a * $scope.b;
-  }; //manual watcher for c
-
+  $scope.d = 40; // $scope.updateC = function(){
+  //     $scope.c = $scope.a * $scope.b
+  // }
+  //manual watcher for c
 
   $scope.$watch("c", function (newVal, oldVal) {
     if (newVal != oldVal) console.log("Updated C to " + newVal);
   });
-  $scope.$watch("a", function (newVal, oldVal) {
-    if (newVal != oldVal) $scope.c = $scope.b * 2;
-  });
-  $scope.$watch("b", function (newVal, oldVal) {
-    if (newVal != oldVal) $scope.b = $scope.a * 2;
-  });
+  $scope.$watchGroup(["a", "b"], function (newVal, oldVal) {
+    console.log(oldVal + " " + newVal);
+    if (newVal != oldVal) $scope.c = $scope.b * $scope.a;
+  }); //     $scope.$watch("a", function(newVal, oldVal){
+  //         if(newVal != oldVal)
+  //             $scope.c = $scope.b * 2
+  //     })
+  //     $scope.$watch("b", function(newVal, oldVal){
+  //         if(newVal != oldVal)
+  //             $scope.b = $scope.a * 2
+  // });
 });
--- AngularJSDemos/watcher.js
+++ AngularJSDemos/watcher.js
@@ -7,23 +7,29 @@ myApp.controller("myController", function($scope){
     $scope.d = 40
 
 
-    $scope.updateC = function(){
-        $scope.c = $scope.a * $scope.b
-    }
+    // $scope.updateC = function(){
+    //     $scope.c = $scope.a * $scope.b
+    // }
 
     //manual watcher for c
     $scope.$watch("c", function(newVal, oldVal){
         if(newVal != oldVal)
             console.log("Updated C to "+ newVal)
-    })
-    $scope.$watch("a", function(newVal, oldVal){
+    });
+    $scope.$watchGroup(["a","b"], function(newVal, oldVal){
+        console.log(oldVal+" "+newVal)
         if(newVal != oldVal)
-            $scope.c = $scope.b * 2
-    })
-    $scope.$watch("b", function(newVal, oldVal){
-        if(newVal != oldVal)
-            $scope.b = $scope.a * 2
-});
+             $scope.c = $scope.b * $scope.a
+    });
+
+//     $scope.$watch("a", function(newVal, oldVal){
+//         if(newVal != oldVal)
+//             $scope.c = $scope.b * 2
+//     })
+//     $scope.$watch("b", function(newVal, oldVal){
+//         if(newVal != oldVal)
+//             $scope.b = $scope.a * 2
+// });
 
 })
 
