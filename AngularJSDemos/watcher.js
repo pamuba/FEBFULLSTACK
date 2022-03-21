801a61513fe0c9b0bec37bb860c2e5a3784a4283
pamuba <partha.bora24@gmail.com>
2022-03-21 19:42:32 +0000

    81

---
+++ AngularJSDemos/watcher.js
@@ -0,0 +1,12 @@
+var myApp = angular.module("myModule",[])
+
+myApp.controller("myController", function($scope){
+    $scope.a = 10
+    $scope.b = 20
+    $scope.c = 30
+    $scope.d = 40
+
+    
+});
+
+
