6a6aebf0c95be104d1980eec47bfad6434237740
pamuba <partha.bora24@gmail.com>
2022-03-22 21:01:40 +0000

    922

---
+++ AngularJSDemos/routeParams.js
@@ -0,0 +1,26 @@
+var myApp = angular.module("myModule",['ngRoute'])
+
+myApp.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+        .when('/sumurl/:a/:b', {
+            templateUrl:'sumurl.html',
+            controller:'sumurl'
+        })
+        .when('/suminput', {
+            templateUrl:'suminput.html',
+            controller:'suminput'
+        })
+        .when('/',{
+            template:'<h1>Welome to the Site</h1>'
+        })
+        .otherwise({
+            template:'<h1>404...Click on the Links</h1>'
+        })
+
+}])
+
+myApp.controller('sumurl', ['$scope','$routeParams', function($scope, $routeParams){
+   $scope.a = $routeParams.a;
+   $scope.b = $routeParams.b;
+}])
+
