c7c9106f1eb7d0a1508fd1ace4f068a3b2367785
pamuba <partha.bora24@gmail.com>
2022-03-24 19:45:11 +0000

    101

--- MEANJS/contactlistapp/public/controllers/controller.js
+++ MEANJS/contactlistapp/public/controllers/controller.js
@@ -1,3 +1,9 @@
-function AppCtrl(){
+function AppCtrl($scope, $http){
     console.log("Controller")
+
+    $http.get("/contactlist").success(function(response){
+        $scope.contactlist = response
+    })
+
+    // $scope.contactlist = contactlist
 }
--- MEANJS/contactlistapp/server.js
+++ MEANJS/contactlistapp/server.js
@@ -8,7 +8,33 @@ var app = express();
 // })
 
 
+
 app.use(express.static(__dirname+'/public'))
+
+
+//routes
+app.get('/contactlist', function(req, res){
+    console.log("Express Server received the get call from Angular")
+
+    person1 = {
+        name : "John",
+        email : "john@gmail.com",
+        number : "(111) 111-1111"
+    }
+    person2 = {
+        name : "Jill",
+        email : "jill@gmail.com",
+        number : "(222) 222-2222"
+    }
+    person3 = {
+        name : "Mill",
+        email : "mill@gmail.com",
+        number : "(333) 333-333"
+    }
+    var contactlist = [person1, person2, person3]
+
+    res.json(contactlist)
+})
 
 app.listen(3000, function(err){
     if(!err){
