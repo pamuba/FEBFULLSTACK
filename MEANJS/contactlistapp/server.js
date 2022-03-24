72192320b4f0795eb162a15066d29a3c54b7f3de
pamuba <partha.bora24@gmail.com>
2022-03-24 20:03:20 +0000

    102

--- MEANJS/contactlistapp/server.js
+++ MEANJS/contactlistapp/server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var db = mongojs('contactlist', ['contactlist'])
 
 var app = express();
 
@@ -16,24 +18,13 @@ app.use(express.static(__dirname+'/public'))
 app.get('/contactlist', function(req, res){
     console.log("Express Server received the get call from Angular")
 
-    person1 = {
-        name : "John",
-        email : "john@gmail.com",
-        number : "(111) 111-1111"
-    }
-    person2 = {
-        name : "Jill",
-        email : "jill@gmail.com",
-        number : "(222) 222-2222"
-    }
-    person3 = {
-        name : "Mill",
-        email : "mill@gmail.com",
-        number : "(333) 333-333"
-    }
-    var contactlist = [person1, person2, person3]
-
-    res.json(contactlist)
+    db.contactlist.find(function(err, docs){
+        if(docs != null)
+            res.json(docs)
+        else{
+            console.log(err)
+        }
+    })
 })
 
 app.listen(3000, function(err){
