d6f5459b949569ade96ff3ab09f2505c6ec141a3
pamuba <partha.bora24@gmail.com>
2022-03-25 19:54:17 +0000

    113

--- NodeJS/fs.js
+++ NodeJS/fs.js
@@ -1,12 +1,37 @@
 let fs = require('fs')
 
+//sync
+// try{
+//     let data = fs.readFileSync('readMe.txt', 'utf-8')
+//     console.log(data)
+//     fs.writeFileSync('writeMe.txt', data)
+// }
+// catch(er){
+//     console.log(er.message)
+// }
+
+
 //async
-try{
-    let data = fs.readFileSync('readMe.txt', 'utf-8')
-    console.log(data)
-    fs.writeFileSync('writeMe.txt', data)
-}
-catch(er){
-    console.log(er.message)
-}
+//try catch === callbacks
+// fs.readFile('readMe.txt', 'utf-8', function(err, data){
+//     if(err === null){
+//         console.log(data)
+//         fs.writeFile('writeMe.txt', data, function(err){
+//             if(err != null)
+//             {
+//                 console.log(err.message)
+//             }
+//             else{
+//                 console.log("The File Written Successfully")
+//             }
+//         })
+//     }
+//     else{
+//         console.log(err.message)
+//     }
+// })
+
+
+
+
 
