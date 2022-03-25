1b1f6da739764cf4e3682f28eaf524a30abb3193
pamuba <partha.bora24@gmail.com>
2022-03-25 20:21:34 +0000

    117

---
+++ NodeJS/server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const fs = require('fs')
+
+// let server = http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type':'text/html'})
+//     res.end("<h2>Hello World<h2>"+" "+req.url)
+// });
+
+// server.listen(3000)
+
