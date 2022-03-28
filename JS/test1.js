c263db397b56494c7ea1511b3c5285b5f40a7f89
pamuba <partha.bora24@gmail.com>
2022-03-28 20:52:01 +0100

    121

---
+++ JS/test1.js
@@ -0,0 +1,17 @@
+let calc2 = (z) => 2 * 10;
+
+let calc1 = (x, y) => {
+  return (x * y) + calc2 (x) 
+}
+
+// console.log(calc1(2,3))
+
+// let values = [2,3]; 
+// console.log(calc1.apply(values));
+
+// console.log(calc1.call(2,3))
+
+// console.log(calc1.call(this, 2,3))
+
+// let values = [2,3]; 
+// console.log(calc1.apply(this, values));
