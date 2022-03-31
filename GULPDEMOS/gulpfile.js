f5b2d171c163daa4c4204612a6142d8ecc8fbe04
pamuba <partha.bora24@gmail.com>
2022-03-31 21:52:56 +0100

    142

--- GULPDEMOS/gulpfile.js
+++ GULPDEMOS/gulpfile.js
@@ -1,5 +1,8 @@
 let gulp = require('gulp')
 let jshint = require('gulp-jshint')
+let cleanCSS = require('gulp-clean-css')
+let rename = require('gulp-rename')
+let sass = require('gulp-sass')
 
 //gulp --tasks lists all tasks
 //gulp task-name to run a task
@@ -19,18 +22,18 @@ let jshint = require('gulp-jshint')
 // })
 
 //creating a task with new gulp ver 4
-function task_jshint(done){
-    gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    done()
-}
-function task_jshint1(done){
-    gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    done()
-}
+// function task_jshint(done){
+//     gulp.src('js/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'))
+//     done()
+// }
+// function task_jshint1(done){
+//     gulp.src('js/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'))
+//     done()
+// }
 
 //named task
 // exports.task_jshint =  task_jshint
@@ -42,4 +45,35 @@ function task_jshint1(done){
 // exports.default = gulp.series(task_jshint, task_jshint1)
 // exports.default = gulp.parallel(task_jshint, task_jshint1)
 
-exports.both = gulp.series(task_jshint, task_jshint1)
+// exports.both = gulp.series(task_jshint, task_jshint1)
+
+
+//////////////////////////////////////////////////////////////
+
+
+//gulp minify + rename css files
+// const styleSRC = './src/scss/style.css'
+// const styleDEST = './dist/css'
+
+// gulp.task('styles', function(done){
+//     gulp.src(styleSRC)
+//         .pipe(cleanCSS())
+//         .pipe(rename({suffix:'.min'}))
+//         .pipe(gulp.dest(styleDEST))
+//     done()
+// });
+
+
+// gulp.task('minify-css', (done) => {
+//     gulp.src(styleSRC)
+//       .pipe(cleanCSS({debug: true}, (details) => {
+//         console.log(`${details.name}: ${details.stats.originalSize}`);
+//         console.log(`${details.name}: ${details.stats.minifiedSize}`);
+//       }))
+//     .pipe(rename({suffix:'.min'}))
+//     .pipe(gulp.dest(styleDEST))
+//     done()
+// });
+
+//////////////////////////////////////////////////////////////////////////
+
