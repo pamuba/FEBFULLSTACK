f74a036ebca280c4b6e6aea08c0c134f3bb3571e
pamuba <partha.bora24@gmail.com>
2022-04-21 21:45:04 +0100

    24444

--- TDD/controllers/todo.controller.js
+++ TDD/controllers/todo.controller.js
@@ -1,6 +1,6 @@
 const TodoModel = require('../model/todo.model')
 
-exports.createTodo = async (req, res, next) => {
+exports.createTodo =  async (req, res, next) => {
   const createdModel = await TodoModel.create(req.body); //201 , res
   res.status(201).json(createdModel);
 }
