6241dc19662ac91b3f73041e16d4dacce27dac25
pamuba <partha.bora24@gmail.com>
2022-05-10 19:30:26 +0100

    341

--- graphqlDemls/users/schema/schema.js
+++ graphqlDemls/users/schema/schema.js
@@ -80,8 +80,30 @@ const mutation = new GraphQLObjectType({
                         .then(res=>res.data)
             }
         },
-        deleteUser:{},
-        updateUser:{}
+        deleteUser:{
+            type:UserType,
+            args:{
+                id:{type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, {id}){
+                return  axios.delete(`http://localhost:3000/users/${id}`)
+                              .then(res => res.data)
+            }
+
+        },
+        updateUser:{
+            type: UserType,
+            args:{
+                id:{type: new GraphQLNonNull(GraphQLString)},
+                firstName:{type: GraphQLString},
+                age:{type:GraphQLInt},
+                companyId:{type: GraphQLString},
+            },
+            resolve(parentValue, args){
+                return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                            .then(res=>res.data)
+            }
+        }
 
     }
 })
