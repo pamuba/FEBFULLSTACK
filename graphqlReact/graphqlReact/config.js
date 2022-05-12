9f5e6a8e83f49a78a9cc523a895e62eb22233393
pamuba <partha.bora24@gmail.com>
2022-05-12 20:50:50 +0100

    364

---
+++ graphqlReact/graphqlReact/config.js
@@ -0,0 +1,1 @@
+export const MONGO_URI = "mongodb+srv://partha_24:partha24@cluster0.xadb5.mongodb.net/graphqldb?retryWrites=true&w=majority"
--- graphqlReact/graphqlReact/server.js
+++ graphqlReact/graphqlReact/server.js
@@ -1,6 +1,25 @@
 import {ApolloServer} from 'apollo-server'
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
 import typeDefs from './schemaGql.js'
+import mongoose from 'mongoose'
+import {MONGO_URI} from './config'
+
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+
+mongoose.connection.on("connected", ()=>{
+  console.log("connected to mongodb")
+})
+
+mongoose.connection.on("error", (err)=>{
+  console.log("error connecting", err)
+})
+
+//import models
+
 import resolvers from './resolvers.js'
 
 const server = new ApolloServer({ 
